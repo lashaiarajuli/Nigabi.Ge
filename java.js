ecb82299db722d6608635dd062adce39195218c3
Lasha Iarajuli <lashaiarajuli300@gmail.com>
2025-11-29 15:50:41 +0400

    Add JS

--- java.js
+++ java.js
@@ -1,7 +1,30 @@
-const button = document.querySelector('.menu');
-const menuNav = document.querySelector('.nav-bar');
+document.addEventListener("DOMContentLoaded", function () {
+    const menuA = document.getElementById("menuA"); // bars
+    const menuB = document.getElementById("menuB"); // close
+    const navList = document.getElementById("navList");
+    const nav = document.querySelector("nav");
 
-button.addEventListener('click', () => {
-    menuNav.classList.toggle('active');
+    // მენიუს გახსნა
+    menuA.addEventListener("click", () => {
+        navList.classList.add("open");
+        menuA.style.display = "none";
+        menuB.style.display = "flex";
+    });
+
+    // მენიუს დახურვა (#menuB)
+    menuB.addEventListener("click", () => {
+        closeMenu();
+    });
+
+    // ნავიგაციაზე (ul, li, a) დაჭერით დამალვა
+    navList.addEventListener("click", () => {
+        closeMenu();
+    });
+
+    // ფუნქცია: მენიუს დახურვა
+    function closeMenu() {
+        navList.classList.remove("open");
+        menuA.style.display = "flex";
+        menuB.style.display = "none";
+    }
 });
-
